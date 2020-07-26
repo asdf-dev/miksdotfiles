0830e5f7478ddc281b251f48fe3d796b669db567
asdf-dev <47029234+asdf-dev@users.noreply.github.com>
2020-07-27 01:46:30 +0200

    typo fix

--- scripts/bash-setup.bash
+++ scripts/bash-setup.bash
@@ -7,7 +7,7 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 bash ~/.bash_it/install.sh
 
 #copy theme to bash
-rm -rf ~/bash_it/themes/bobby
+rm -rf ~/.bash_it/themes/bobby
 cp -r ~/Desktop/MiksDotfiles/bobby/ ~/.bash_it/themes
 
 #copy alias
