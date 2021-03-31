13ee133690c2b0bab76642a8e595bf423db2b8da
asdf-dev <47029234+asdf-dev@users.noreply.github.com>
2021-03-31 14:41:54 +0200

    added links

---
+++ links.sh
@@ -0,0 +1,5 @@
+open https://www.gitkraken.com/
+open https://www.microsoft.com/da-dk/microsoft-teams/download-app
+open https://www.google.com/intl/da/chrome/
+open https://snapcraft.io/authy
+open https://discord.com/
