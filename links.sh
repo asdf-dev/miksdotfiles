7a68c42ce0c3ab88a2b7e842b32eeac446ff3c86
asdf-dev <47029234+asdf-dev@users.noreply.github.com>
2021-04-19 09:18:01 +0200

    link.sh changed open to firefox

--- links.sh
+++ links.sh
@@ -1,5 +1,5 @@
-open https://www.gitkraken.com/
-open https://www.microsoft.com/da-dk/microsoft-teams/download-app
-open https://www.google.com/intl/da/chrome/
-open https://snapcraft.io/authy
-open https://discord.com/
+firefox https://www.gitkraken.com/
+firefox https://www.microsoft.com/da-dk/microsoft-teams/download-app
+firefox https://www.google.com/intl/da/chrome/
+firefox https://snapcraft.io/authy
+firefox https://discord.com/
